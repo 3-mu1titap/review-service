09aca033d18e66506ae17a16c8c4d835c6d51a17
Seong-Tae-Kim <rlatjdxo6462@gmail.com>
2024-11-08 10:08:35 +0900

    Feat: 리뷰 softDelete 구현 #6

--- src/main/java/com/multitap/review/review/application/ReviewService.java
+++ src/main/java/com/multitap/review/review/application/ReviewService.java
@@ -1,10 +1,12 @@
 package com.multitap.review.review.application;
 
 import com.multitap.review.review.dto.in.CreateReviewRequestDto;
+import com.multitap.review.review.dto.in.SoftDeleteReviewRequestDto;
 import com.multitap.review.review.dto.in.UpdateReviewRequestDto;
 
 public interface ReviewService {
 
     void createReview(CreateReviewRequestDto createReviewRequestDto);
     void updateReview(UpdateReviewRequestDto updateReviewRequestDto);
+    void softDeleteReview(SoftDeleteReviewRequestDto softDeleteReviewRequestDto);
 }
